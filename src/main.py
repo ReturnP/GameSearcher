174b8b92beaaf7aee71f11f33e5c48a6dd0c02bd
andrikarel <karelandri@gmail.com>
2016-12-12 19:51:18 +0000

    Implemented a working search bar

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
-
-app.run()
+@app.route('/search',methods=['POST'])
+def search():
+    gameTitle = request.form['searchPath']
+    return render_template('search_result.html',gameTitle = gameTitle)
+if __name__ == '__main__':
+    app.run()
