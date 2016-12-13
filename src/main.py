6daa1cf73e6eeb14d949689d67040b036f641a5a
andrikarel <karelandri@gmail.com>
2016-12-13 15:26:26 +0000

    its working, its WORKING

--- src/main.py
+++ src/main.py
@@ -1,23 +1,38 @@
-from flask import Flask,render_template,request,url_for, request
+from flask import Flask,render_template,request,url_for, request,jsonify
 import requests
 import json
 app = Flask(__name__)
 app.config.update(TEMPLATES_AUTO_RELOAD=True)
     
+with open('config.json') as jData:
+    apiKey = json.load(jData)['ApiKey']
 
 @app.route('/')
 def index():
+    
+
     return render_template('index.html')
 
-@app.route('/search',methods=['POST'])
+@app.route('/search')
 def search():
-    with open('config.json') as jData:
-        apiKey = json.load(jData)['ApiKey']
-    gameTitle = request.form['searchPath']
-    response = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=3&offset=0&order=rating_count%3Adesc&search={}".format(gameTitle),headers={"X-Mashape-Key": apiKey,"Accept": "application/json"})
-    title = response.json()[0]["name"]               
-    return render_template('search_result.html',title = title)
 
+    gameTitle = request.args.get("title","NONE",type=str)
+    response = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10&offset=0&order=rating_count%3Adesc&search={}".format(gameTitle),headers={"X-Mashape-Key": apiKey,"Accept": "application/json"})
+    title = response.json()
+    return jsonify(result=title)
+
+
+@app.route('/displayGame/<gameId>',methods=['GET','POST'])
+def displayGame(gameId):
+    response = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/{}?fields=*".format(gameId), headers={
+    "X-Mashape-Key": apiKey,
+    "Accept": "application/json"
+  })
+    
+    title = response.json()[0]['name']
+    cover = response.json()[0]['cover']['url']
+
+    return render_template('search_result.html',title = title,cover=cover)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
