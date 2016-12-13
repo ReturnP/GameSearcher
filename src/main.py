f70cd5e20970d572ba7d29f50b03a6ad34847204
andrikarel <karelandri@gmail.com>
2016-12-13 11:49:30 +0000

    added json and gitignore

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request,url_for, request
-
+import requests
+import json
 app = Flask(__name__)
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
+    
 
 @app.route('/')
 def index():
@@ -8,8 +11,12 @@ def index():
 
 @app.route('/search',methods=['POST'])
 def search():
+    with open('config.json') as jData:
+        apiKey = json.load(jData)['ApiKey']
     gameTitle = request.form['searchPath']
-    return render_template('search_result.html',gameTitle = gameTitle)
+    response = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=3&offset=0&order=rating_count%3Adesc&search={}".format(gameTitle),headers={"X-Mashape-Key": apiKey,"Accept": "application/json"})
+    title = response.json()[0]["name"]               
+    return render_template('search_result.html',title = title)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,6 +28,8 @@ def login():
         else:
             return index()#TODO: change this to redirect?
     return render_template('login.html', error=error)
+
+
 
 if __name__ == '__main__':
     app.run()
