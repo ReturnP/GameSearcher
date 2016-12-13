0a9b28e31fab6de725bbc175cb081be11240680c
Smari <smarig15@ru.is>
2016-12-13 18:42:42 +0000

    Added check for rating and summary; Added document(ready) funcvtion to javascript

--- src/main.py
+++ src/main.py
@@ -9,13 +9,10 @@ with open('config.json') as jData:
 
 @app.route('/')
 def index():
-    
-
     return render_template('index.html')
 
 @app.route('/search')
 def search():
-
     gameTitle = request.args.get("title","NONE",type=str)
     response = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=5&offset=1&order=name&order=rating_count&order=popularity%3Adesc&search={}".format(gameTitle),headers={"X-Mashape-Key": apiKey,"Accept": "application/json"})
     title = response.json()
@@ -27,14 +24,20 @@ def displayGame(gameId):
     response = requests.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/{}?fields=*".format(gameId), headers={
     "X-Mashape-Key": apiKey,
     "Accept": "application/json"
-  })
-    
-    title = response.json()[0]['name']
-    cover = response.json()[0]['cover']['url']
-    summary = response.json()[0]['summary']#TODO: make empty if game has no summary
-    rating = response.json()[0]['rating']
-    rating = rating
-    rating = str(("%.2f" % int(rating)))
+    })
+
+    info = response.json()[0]
+    title = info['name']
+    cover = info['cover']['url']
+    if 'summary' in info:
+        summary = info['summary']
+    else:
+        summary = 'No summary'
+    if 'rating' in info:
+        rating = info['rating']
+        rating = str(("%.2f" % int(rating)))
+    else:
+        rating = 'No rating'
 
     return render_template('search_result.html',title = title,cover=cover,summary = summary,rating = rating)
 
